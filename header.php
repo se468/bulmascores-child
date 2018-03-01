19168b98aa581776f84915ab7b26b872c72afe0a
Seyong Cho <se468@hotmail.com>
2018-03-01 11:04:04 +0900

    navbar logo

--- header.php
+++ header.php
@@ -10,21 +10,22 @@
  */
 
 ?>
-
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+
 	
+
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri();?>/dist/app.css">
 	
 	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
 	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/dist/vendor.js"></script>
 	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/dist/app.js"></script>
-
+	
 
 	<?php wp_head(); ?>
 </head>
@@ -34,16 +35,24 @@
 	
 
 	<div id="page" class="site">
-
+		
 		<header id="masthead" class="site-header">
 			<nav class="navbar is-info">
 				<div class="container">
 					<div class="navbar-brand">
-						<a class="navbar-item" href="/">
 
-							<img src="https://bulma.io/images/bulma-logo-white.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
 
-						</a>
+						<!-- Your site title as branding in the menu -->
+						<?php if ( ! has_custom_logo() ) { ?>
+							<a class="navbar-item" href="/">
+								<b>Bulmascores</b>
+							</a>
+						<?php } else { 
+							the_custom_logo();
+						} ?><!-- end custom logo -->
+
+
+						
 						<div class="navbar-burger burger" data-target="navMenuColorinfo-example">
 							<span></span>
 							<span></span>
@@ -66,11 +75,7 @@
 
 						<div class="navbar-end">
 							<a class="navbar-item" href="https://github.com/se468/Bulmascores">
-								<i class="fab fa-github"></i> &nbsp;
-								Github
-							</a>
-							<a class="navbar-item" href="https://github.com/se468/Bulmascores">
-								Demo
+								<i class="fab fa-github"></i>
 							</a>
 						</div>
 					</div>
@@ -78,5 +83,6 @@
 			</nav>
 			<!-- #bulma-site-navigation -->
 		</header><!-- #masthead -->
+
 
 		<div id="content" class="site-content">
